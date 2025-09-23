fc42db52515963f9986917cfb70af8d5eef7c3b4
woonwai359 <Patnareeja062@gmail.com>
2025-09-23 14:02:59 +0700

    fix quasar again

--- quasar.config.js
+++ quasar.config.js
@@ -35,7 +35,7 @@ export default defineConfig((ctx) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#build
     build: {
-      publicPath: '/VersionControl2/',
+      publicPath: '/versionControl2/',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // webpackTranspile: false,
